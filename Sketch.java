ec082574e6ab87ea9f6d393963b52b7482fbbb42
Preston-Wong <you@example.com>
2023-09-28 09:28:01 -0400

    Updated Styling

--- Sketch.java
+++ Sketch.java
@@ -5,19 +5,11 @@ import processing.core.PApplet;
  */
 public class Sketch extends PApplet {
 	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
   public void settings() {
-	// put your size call here
+	  // Put your size call here
     size(800, 800);
   }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
   public void setup() {
     background(	135, 206, 235);
 
